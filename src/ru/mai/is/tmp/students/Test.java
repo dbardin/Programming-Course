7fac756c707536aca7198f1f63850a5718494203
dbardin <dbardin@croc.ru>
2013-02-20 18:57:23 +0400

    add students test class

---
+++ src/ru/mai/is/tmp/students/Test.java
@@ -0,0 +1,11 @@
+package ru.mai.is.tmp.students;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rdbardin
+ * Date: 20.02.13
+ * Time: 18:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
