385e6edc9d56ae9e624ac229e51cf9af996c2162
dbardin <dbardin@croc.ru>
2013-02-20 18:58:42 +0400

    change test class

--- src/ru/mai/is/tmp/students/Test.java
+++ src/ru/mai/is/tmp/students/Test.java
@@ -2,7 +2,7 @@ package ru.mai.is.tmp.students;
 
 /**
  * Created with IntelliJ IDEA.
- * User: rdbardin
+ * User: dbardin
  * Date: 20.02.13
  * Time: 18:57
  * To change this template use File | Settings | File Templates.
