6d4a51e2d496705eec2cebd4e3de206f3843e570
dbardin-new <dbardin@croc.ru>
2013-03-20 18:01:50 +0400

    change

--- src/main/java/ru/mai/is/tmp/students/example/oop/inheritance/inheritanceAbstract/IPhone.java
+++
@@ -1,8 +0,0 @@
-package ru.mai.is.tmp.students.example.oop.inheritance.inheritanceAbstract;
-
-/**
- * @author dbardin
- */
-public class IPhone extends MobilePhone {
-}
-
--- src/main/java/ru/mai/is/tmp/students/example/oop/inheritance/inheritanceAbstract/OldNokia.java
+++
@@ -1,8 +0,0 @@
-package ru.mai.is.tmp.students.example.oop.inheritance.inheritanceAbstract;
-
-/**
- * @author dbardin
- */
-public class OldNokia extends MobilePhone {
-}
-
