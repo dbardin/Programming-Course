9691d52d25ff3cc95b9af036750737e8fec555a3
R-Gnezdilov <rmn-gnezdo@mail.ru>
2013-11-01 21:29:03 +0400

    Еще одна попытка создать папку R-Gnezdilov и файл с hello
    Еще одна попытка создать папку R-Gnezdilov и файл с hello внутри папки

---
+++ src/main/java/ru/mai/is/tmp/students/R-Gnezdilov/JavaApplication1.java
@@ -0,0 +1,16 @@
+package javaapplication1;
+
+/**
+ *
+ * @author R-Gnezdilov
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    
+}
