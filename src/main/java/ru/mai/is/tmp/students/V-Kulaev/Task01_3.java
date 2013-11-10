71c260d1389dbfb2bfd6ce5c1f6cc4a97682bfde
JackSouth <kulvlad9318@gmail.com>
2013-11-10 22:28:10 +0400

    Домашка номер 00 и 01 задания со 2 по 5
    попытка отправить

---
+++ src/main/java/ru/mai/is/tmp/students/V-Kulaev/Task01_3.java
@@ -0,0 +1,19 @@
+﻿/**
+ * Created with IntelliJ IDEA.
+ * User: V-Kulaev
+ * Date: 10.11.13
+ * Time: 10:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class Homework01_3_1 {
+    public static void main(String[] args){
+        Input t=new Input();
+        String s=t.getUserInput("Введите число");
+        int n=Integer.parseInt(s);
+        if (n%2==0) {
+            System.out.println("Число четное");
+        } else {
+            System.out.println("Число нечетное");
+        }
+    }
+}
