4b78720046d1135decccbc6fa2a4622bdd3fa3ce
arybalko <kokosls@yandex.ru>
2013-11-12 21:48:54 +0400

    ДомЗад01 Рыбалко А.

---
+++ src/main/java/ru/mai/is/tmp/students/arybalko/Summa.java
@@ -0,0 +1,22 @@
+/**
+ * @author arybalko
+ * Программа считает сумму цифр числа
+ * Task_01 Задача 2
+ */
+import java.util.Scanner;
+public class Summa {
+	public static void main(String[] args) {
+		System.out.print("Введите натуральное число: ");
+		Scanner scan= new Scanner(System.in);
+		int n = scan.nextInt();
+		int s=0;
+		int a;
+		while (n>0) {
+			a=n%10;
+			n=n/10;
+			s=s+a;
+		}
+		System.out.print("Сумма цифр введенного числа: "+s);	
+	}
+}
+//Программа работает не только с трехзначными числами, благодаря циклу while
