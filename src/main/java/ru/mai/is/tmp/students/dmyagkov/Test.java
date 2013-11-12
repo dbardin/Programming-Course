8cbc3d516caf0d7073aebb2aca865986542f91cd
D800 <dima.myagkov1@yandex.ru>
2013-11-12 19:26:36 +0400

    домашнее задание Мягков Д

---
+++ src/main/java/ru/mai/is/tmp/students/dmyagkov/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: DIMA
+ * Date: 25.10.13
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello !");
+    }
+}
