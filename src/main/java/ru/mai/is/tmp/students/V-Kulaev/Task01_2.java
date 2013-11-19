ac32826d53b46ac30e048da07c578c6f6c1134bc
JackSouth <kulvlad9318@gmail.com>
2013-11-19 20:57:40 +0400

    дз исправленный и 01_1

--- src/main/java/ru/mai/is/tmp/students/V-Kulaev/Task01_2.java
+++ src/main/java/ru/mai/is/tmp/students/V-Kulaev/Task01_2.java
@@ -9,6 +9,17 @@ public class Homework01_2_1 {
     public static void main(String[] args){
         Input t=new Input();
         String s=t.getUserInput("Введите число");
+<<<<<<< HEAD
+        int CelayaChOtDel=Integer.parseInt(s);
+        int OstatokOtDel;
+        int Summa=0;
+        while (CelayaChOtDel>0) {
+            OstatokOtDel=CelayaChOtDel%10;
+            Summa=Summa+OstatokOtDel;
+            CelayaChOtDel=CelayaChOtDel/10;
+        }
+        System.out.println(Summa);
+=======
         int n=Integer.parseInt(s);
         int c;
         int S=0;
@@ -18,5 +29,6 @@ public class Homework01_2_1 {
             n=n/10;
         }
         System.out.println(S);
+>>>>>>> 4f5d95edd47ca369ff0ab1632ed6a1bf6c339da1
     }
 }
