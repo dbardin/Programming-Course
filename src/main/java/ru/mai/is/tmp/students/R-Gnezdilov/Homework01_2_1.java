c35d378b0bb6a0b025b65c6e7fe601ce7136affa
R-Gnezdilov <rmn-gnezdo@mail.ru>
2013-11-20 10:20:59 +0400

    Дз исправленное с осмысленными переменными

---
+++ src/main/java/ru/mai/is/tmp/students/R-Gnezdilov/Homework01_2_1.java
@@ -0,0 +1,22 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: SuPeRhEr0
+ * Date: 10.11.13
+ * Time: 15:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class Homework01_2_1 {
+    public static void main(String[] args){
+        Input t=new Input();
+        String stroka=t.getUserInput("Введите число");
+        int chislo=Integer.parseInt(stroka);
+        int OstatokOtDeleniya;
+        int Summa=0;
+        while (chislo>0) {
+            OstatokOtDeleniya=chislo%10;
+            Summa=Summa+OstatokOtDeleniya;
+            chislo=chislo/10;
+        }
+        System.out.println(Summa);
+    }
+}
