90f69dcfde4abc94752aa30428424fb9aab8a2b6
elinasuvorova <elinatt@rambler.ru>
2013-12-09 18:52:36 +0400

    Task01

---
+++ src/main/java/ru/mai/is/tmp/students/e_teplinskala/Task01/Fibonachi.java
@@ -0,0 +1,14 @@
+package ru.mai.is.tmp.students.e_teplinskala.Task01;
+public class Fibonachi {
+    public static void main (String args[]) {
+        int p,f1=1,f2=1;
+        System.out.println(f1);
+        System.out.println(f2);
+        for (int i=3; i<11; i++){
+            f=f1+f2;
+            System.out.println(f);
+            f1=f2;
+            f2=f;}
+
+    }
+}
