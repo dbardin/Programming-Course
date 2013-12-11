9e9e990435f6d0588ef54671e2dea02e0054b423
D800 <dima.myagkov1@yandex.ru>
2013-12-11 21:47:14 +0400

    Д.з. 01 исправленое

--- src/main/java/ru/mai/is/tmp/students/dmyagkov/task 01/Interval.java
+++ src/main/java/ru/mai/is/tmp/students/dmyagkov/task 01/Interval.java
@@ -1,18 +1,25 @@
-package interval;
+package ru.mai.is.tmp.students.dmyagkov.task01;
+/*
+*Stroki
+*
+*Программа определяет входит ли число в интервал
+*
+*11.12.13
+*
+*@author Мягков Дмитрий
+*/
 import java.util.*;
-
 public class Interval {
-    
     public static void main(String[] args) {
         int number;
         System.out.println("Введите  число");
         Scanner scn= new Scanner(System.in);
         number=scn.nextInt();
-         if ((number>5)&&(number<555)){
-             System.out.println("Число входит в интервал(5;555)");
-            }
-         else{ 
-             System.out.println("Число не входит в интервал(5;555)");
+        if ((number>5)&&(number<555)){
+            System.out.println("Число входит в интервал(5;555)");
+        }
+        else{ 
+            System.out.println("Число не входит в интервал(5;555)");
         } 
     }
 }
