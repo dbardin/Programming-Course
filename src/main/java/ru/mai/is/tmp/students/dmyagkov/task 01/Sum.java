d6ce3f3e51ca6dc33221d724221857f24f8383a1
D800 <dima.myagkov1@yandex.ru>
2013-12-11 21:47:14 +0400

    Д.з. 01 исправленое

--- src/main/java/ru/mai/is/tmp/students/dmyagkov/task 01/Sum.java
+++ src/main/java/ru/mai/is/tmp/students/dmyagkov/task 01/Sum.java
@@ -1,19 +1,26 @@
-package sum;
+package ru.mai.is.tmp.students.dmyagkov.task01;
+/*
+*Stroki
+*
+*Программа выводит сумму цифр числа
+*
+*11.12.13
+*
+*@author Мягков Дмитрий
+*/
 import java.util.*;
 public class Sum {
-  
     public static void main(String[] args) {
     int sum=0;
     int number,ostatok;
     System.out.println("Введите трехзначное число");
     Scanner scn= new Scanner(System.in);
     number=scn.nextInt();
-     while(number>0){
+    while(number>0){
         ostatok=number%10; 
         sum=sum+ostatok;
         number=number/10;
-      }
+    }
     System.out.println(sum);
     }
-    
 }
