072fad8cc1e0a1f2acecdebb2c89489b62200b05
D800 <dima.myagkov1@yandex.ru>
2013-12-11 21:58:48 +0400

    еще исправление

--- src/main/java/ru/mai/is/tmp/students/dmyagkov/task 01/Chetnost.java
+++ src/main/java/ru/mai/is/tmp/students/dmyagkov/task 01/Chetnost.java
@@ -9,16 +9,17 @@ package ru.mai.is.tmp.students.dmyagkov.task01;
 *@author Мягков Дмитрий
 */
 import java.util.*;
-
 public class Chetnost {
-
     public static void main(String[] args) {
         int number;
         System.out.println("Введите число");
         Scanner scn= new Scanner(System.in);
         number=scn.nextInt();
-        if (number%2==0){System.out.println(number+"-четное");}
-            else System.out.println(number+"-нечетное");
+        if (number%2==0){
+            System.out.println(number+"-четное");
+        }else {
+            System.out.println(number+"-нечетное");
+        }
          
     }
     
