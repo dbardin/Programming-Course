5e6079133bbc50298390629ef21f1f6007a5b788
R-Gnezdilov <rmn-gnezdo@mail.ru>
2013-12-11 19:42:14 +0400

    Дз иерархия и курсач

--- src/main/java/ru/mai/is/tmp/students/R-Gnezdilov/Hierarchy/Car.java
+++ src/main/java/ru/mai/is/tmp/students/R-Gnezdilov/Hierarchy/Car.java
@@ -1,8 +1,12 @@
+package ru.mai.is.tmp.students.R-Gnezdilov.Hierarchy;
+
 abstract class Car {
     protected int Speed;
     protected int Capacity;
-    void AccelerateTheCar(){
+
+    void AccelerateTheCar() {
         System.out.println("Машина разогнана до 100км/ч за 20 секунд");
     }
+
     abstract void CrashTest();
 }
--- src/main/java/ru/mai/is/tmp/students/R-Gnezdilov/Hierarchy/Scania.java
+++ src/main/java/ru/mai/is/tmp/students/R-Gnezdilov/Hierarchy/Scania.java
@@ -1,4 +1,6 @@
-class Scania extends Truck  {
+package ru.mai.is.tmp.students.R-Gnezdilov.Hierarchy;
+
+class Scania extends Truck {
     @Override
     void CrashTest() {
         System.out.println("Crash-test пройден успешно, никто не пострадал");
