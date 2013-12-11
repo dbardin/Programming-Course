6d7a21ca0775c409e015cf30db2049344c02058c
D800 <dima.myagkov1@yandex.ru>
2013-12-11 22:10:44 +0400

    Д.з.02

---
+++ src/main/java/ru/mai/is/tmp/students/dmyagkov/task02/Circle.java
@@ -0,0 +1,8 @@
+package ru.mai.is.tmp.students.dmyagkov.task02;
+/**
+ *для круглых фигур 
+ */
+interface  Circle {
+    double getCircleLength(double a,double b);
+    double getRadius(double a,double b);
+}
