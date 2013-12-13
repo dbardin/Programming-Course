f642fb25b5ac0b42ad7e4ea97e54607053bbf600
elinasuvorova <elinatt@rambler.ru>
2013-12-13 19:46:14 +0400

    Task02 (исправленное)

---
+++ src/main/java/ru/mai/is/tmp/students/e_teplinskala/dom.rabota/Task02/Berry_grade.java
@@ -0,0 +1,7 @@
+package ru.mai.is.tmp.students.e_teplinskala.dom_rabota.Task02;
+abstract  public class Berry_grade {
+    int  Volume;
+    int  Quantity_of_stones;
+    String Type;
+    String existence_of_stones;
+}
---
+++ src/main/java/ru/mai/is/tmp/students/e_teplinskala/dom.rabota/Task02/Sweet_berries.java
@@ -0,0 +1,2 @@
+package ru.mai.is.tmp.students.e_teplinskala.dom_rabota.Task02;
+abstract public class Sweet_berries extends Berry_grade {}
