51aae744551165003c2efe95072f252c3616c5e4
UsachevaNatalia <esperansa39@mail.ru>
2013-12-13 16:33:18 +0400

    ДЗ

---
+++ src/main/java/ru/mai/is/tmp/students/nusacheva/hometask/Ierarhia/Wolf.java
@@ -0,0 +1,17 @@
+package ru.mai.is.tmp.students.nusacheva.hometask.Ierarhia;
+public class Wolf extends Predatory implements Animals{
+    private String colorOfWool;
+    public String getColorOfWool(){
+        return colorOfWool;
+    }
+    public void setColorOfWool (String colorOfWool){
+        if(colorOfWool.contains("cерый")){
+            this.colorOfWool=colorOfWool;
+        }
+        else this.colorOfWool="cерый";
+
+    }
+    public void kindOfAnimal(){
+        System.out.println("Перед вами " +getColorOfWool()+ " волк. Среда обитания - " +getHabitat());
+    }
+}
