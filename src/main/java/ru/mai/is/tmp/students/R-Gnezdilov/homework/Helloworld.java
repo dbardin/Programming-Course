415a7ba17b99ee2b2fd13c95d2eea4826c18dfd7
noskill1 <anton.zabolotnyi@mail.ru>
2013-12-16 17:47:12 +0400

    ДЗ

---
+++ src/main/java/ru/mai/is/tmp/students/R-Gnezdilov/homework/Helloworld.java
@@ -0,0 +1,12 @@
+package ru.mai.is.tmp.students.R-Gnezdilov.homework;
+
+/**
+ * @author R-gnezdilov
+ */
+public class Helloworld {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/ru/mai/is/tmp/students/azabolotnyi/homework/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 16.12.13
+ * Time: 17:07
+ * To change this template use File | Settings | File Templates.
+ */
+package ru.mai.is.tmp.students.azabolotnyi.homework;
+public class HelloWorld {
+public static void main(String[] args){
+System.out.println("Hello World");
+}
+}
