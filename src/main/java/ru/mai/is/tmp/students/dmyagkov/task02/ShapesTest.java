88af60f1c141e0d5c12cf782ba27bb19c1d54de9
D800 <dima.myagkov1@yandex.ru>
2013-12-16 18:13:16 +0400

    исправление

--- src/main/java/ru/mai/is/tmp/students/dmyagkov/task02/ShapesTest.java
+++ src/main/java/ru/mai/is/tmp/students/dmyagkov/task02/ShapesTest.java
@@ -18,22 +18,17 @@ public class ShapesTest {
         height=scn.nextDouble();
         System.out.println("Введите ширину прямоугольника,в который будут вписаны фигуры");
         width=scn.nextDouble();
+        Shape recTri= new RectangularTriangle(); 
+        Shape sq=new Square();
+        Shape ell=new Ellipse();
+        Shape ro=new Round();
         Shape [] myShapes= new Shape[4];
-        RectangularTriangle recTri= new RectangularTriangle(); 
         myShapes[0]=recTri;
-        Square sq=new Square();
         myShapes[1]=sq;
-        Ellipse ell=new Ellipse();
         myShapes[2]=ell;
-        Round ro=new Round();
         myShapes[3]=ro;
         for(Shape figure:myShapes){
             System.out.println(figure.getName()+" имеет площадь:"+figure.getArea(height, width));    
         }
-        System.out.println(recTri.getName()+" имеет периметр:"+recTri.getPerimeter(height, width));
-        System.out.println(ell.getName()+" имеет периметр:"+ell.getCircleLength(height, width));
-        System.out.println("Сумма малого и большого радиусов:"+ell.getRadius(height, width));
-        System.out.println("Длина окружности круга:"+ro.getCircleLength(height, width));
-        System.out.println("Радиус круга:"+ro.getRadius(height, width));
     }  
 }
