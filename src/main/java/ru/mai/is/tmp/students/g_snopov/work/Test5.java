9ba6d82c59cd5dce277c349bbd0d8d53418e2d9a
GS1996 <sarseeditor408@gmail.com>
2013-12-17 06:51:37 -0800

    home work 6(окончательно исправленное)

---
+++ src/main/java/ru/mai/is/tmp/students/g_snopov/work/Test5.java
@@ -0,0 +1,30 @@
+package ru.mai.is.tmp.students.g_snopov.work ;
+
+/**Java 7
+ * NetBeans IDE 7.4
+ * Task_01
+ * Test_05
+ * @author G-Snopov
+ * User GS1996
+ */
+public class Test5 {
+
+   
+    public static void main(String[] args) {
+        //The Fibonacci series is series of numbers where each number is the sum of the two previous matches numbers
+        int i = 1 ; 
+        int i1 = 1 ;
+        int i2 = 0 ; //Greate the first two numbers in the sequence
+        System.out.println("") ;
+       while (i2<10) 
+       {
+          System.out.print(i+"" +i1+"") ;
+           i2 += 2 ;  
+           i += i1 ; 
+           i1 = i + i1 ;
+// A While loop that generates a sequence of 10 integers
+        }
+
+    }
+    
+}
