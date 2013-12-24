80011593609d48113e8b28ffd01c6a26b869ca5d
D800 <dima.myagkov1@yandex.ru>
2013-12-25 01:49:29 +0400

    курсовая

--- src/main/java/ru/mai/is/tmp/students/dmyagkov/coursework/CourseWork.java
+++ src/main/java/ru/mai/is/tmp/students/dmyagkov/coursework/CourseWork.java
@@ -8,6 +8,7 @@ package ru.mai.is.tmp.students.dmyagkov.coursework;
  *
  *@author Мягков Дмитрий
  */
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -21,11 +22,7 @@ public class CourseWork {
         //IOException - Если ошибка ввода-вывода происходит
 
         File t = new File("src\\main\\resources\\Otveti.txt"); //Задание пути к файлу
-        Scanner scn = new Scanner(t, "UTF-8"); //использовал сканер что бы указать кодировку,BufferedReader у меня читал криво 
-        //FileReader fr = new FileReader(t);
-        //BufferedReader pir = new BufferedReader(fr);
-        //Создание объекта под названием BufferedReader. Параметр FileReader, 
-        //а его параметр - это путь к файлу
+        Scanner scn = new Scanner(t, "UTF-8");
         String russian; //Задание переменной
 
         File f = new File("src\\main\\resources\\Slova.txt");
@@ -34,14 +31,13 @@ public class CourseWork {
         String english; //Задание переменной
 
         while ((english = fin.readLine()) != null) {//Цикл прервется, когда будет конец файла
-            //russian = pir.readLine(); //Отправляем переменной сообщение методом readLine
-            russian = scn.nextLine();//использование сканера для чтения файла
+            russian = scn.nextLine();
             System.out.print(english + " - "); //Вывести на экран английское слово
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
             //System.in говорит,что ввод будет производиться с клавиатуры
             slovo = br.readLine(); //Присвоение переменной значение введенное с клавиатуры
             if (slovo.equalsIgnoreCase(russian)) { //Сравнение введенного слова с правильным переводом
-                //сли совпадает
+                //если совпадает
                 System.out.println("Ответ верный"); //Вывести на экран
             } else { //Если не совпадает
                 System.out.println("Ответ неверный" + " правильно:" + russian); //Вывести на экран
---
+++ src/test/java/ru/mai/is/tmp/students/dmyagkov/coursework/CourseWorkTest.java
@@ -0,0 +1,38 @@
+package ru.mai.is.tmp.students.dmyagkov.coursework;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CourseWorkTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CourseWorkTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CourseWorkTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
