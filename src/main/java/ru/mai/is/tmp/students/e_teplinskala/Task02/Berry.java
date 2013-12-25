307bbbe8cffa11a8144fb63addec49b84bfeadc3
elinasuvorova <elinatt@rambler.ru>
2013-12-25 13:57:48 +0400

    Task02

--- src/main/java/ru/mai/is/tmp/students/e_teplinskala/Task02/Berry.java
+++
@@ -1,4 +0,0 @@
-package ru.mai.is.tmp.students.e_teplinskala.Task02;
-public interface Berry {
-public void Creation ();
-}
--- src/main/java/ru/mai/is/tmp/students/e_teplinskala/Task02/Berry_grade.java
+++
@@ -1,8 +0,0 @@
-package ru.mai.is.tmp.students.e_teplinskala.Task02;
-abstract  public class Berry_grade {
-    int  Volume;
-    int  Color;
-    int  Quantity_of_stones;
-    String Type;
-
-}
