8f444652056eeeee2ea06221f10d5d18eceafd22
PKlyuev <advokatru07@mail.ru>
2014-01-18 17:13:34 +0400

    del

--- src/main/java/ru/mai/is/tmp/students/PKlyuev/Homework/t_02/Domestic.java
+++
@@ -1,8 +0,0 @@
-package ru.mai.is.tmp.students.PKlyuev.t_02;
-
-abstract class Domestic implements IAnimals{
-	
-	protected void property() {
-		System.out.println("Animal - Class: wild; Rating: ");
-		}
-	}
